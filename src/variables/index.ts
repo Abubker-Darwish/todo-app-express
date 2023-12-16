c3ba710b4ac3fa2829063118258afcfb28b445a4
Abubker Darwish <abubker.darwish@gmail.com>
2023-12-16 02:47:38 +0200

    fix: variables eslint warning

--- src/variables/index.ts
+++ src/variables/index.ts
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const variables = {
-  port: process.env.PORT || '5000',
-  secret: process.env.SECRET || '',
-  env: process.env.NODE_ENV || '',
-  db_url: process.env.DATABASE_URL || '',
-  cloud_name: process.env.CLOUDINAY_CLOUD_NAME || '',
-  api_key: process.env.CLOUDINAY_API_KEY || '',
-  api_secret: process.env.CLOUDINAY_API_SECRET || '',
+  port: process.env.PORT ?? '5000',
+  secret: process.env.SECRET ?? '',
+  env: process.env.NODE_ENV ?? '',
+  db_url: process.env.DATABASE_URL ?? '',
+  cloud_name: process.env.CLOUDINAY_CLOUD_NAME ?? '',
+  api_key: process.env.CLOUDINAY_API_KEY ?? '',
+  api_secret: process.env.CLOUDINAY_API_SECRET ?? '',
 };
 
 export default variables;
